a5e4dae00521ecb3ec4fa7cd8de4a1b708c6d984
Kelly Crowther <crowther.kelly1@gmail.com>
2019-08-05 21:18:29 -0700

    create formgroup with email validation and required fields; create handleRegistrationSubmision method to retrieve values from form; disable form submission button if form invalid

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+interface ISubscriptionLevel {
+  name: string;
+  value: number;
+  id: number;
+}
 
 @Component({
   selector: 'registration',
@@ -7,9 +14,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
+  public registrationForm: FormGroup = new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    email: new FormControl('', Validators.email),
+    subscriptionLevel: new FormControl()
+  });
+  public subscriptionLevels: Array<ISubscriptionLevel>;
+
   constructor() { }
 
   public ngOnInit(): void {
+    this.createSubscriptionLevels();
+  }
+
+  public handleRegistrationSubmission(): void {
+    console.log('First Name: ' + this.registrationForm.get('firstName').value);
+    console.log('Subscription Level: ', this.registrationForm.get('subscriptionLevel').value);
+    console.info('Form Data: ', this.registrationForm.value);
+  }
+
+  public createSubscriptionLevels(): void {
+    this.subscriptionLevels = [
+      {
+        name: 'Bronze',
+        value: 50,
+        id: 1
+      },
+      {
+        name: 'Silver',
+        value: 100,
+        id: 2
+      },
+      {
+        name: 'Gold',
+        value: 150,
+        id: 3
+      },
+      {
+        name: 'Platinum',
+        value: 200,
+        id: 4
+      }
+    ];
   }
 
 }
